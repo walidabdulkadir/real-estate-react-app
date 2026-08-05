9aac0776628018c48ca69168dc1b7c0e18ef45f8
walidabdulkadir <yesufabdulkadir942@gmail.com>
2026-08-05 18:14:38 +0300

    feat: add AboutCard, AboutList, and Data components for About section

---
+++ frontEnd/src/components/About/Data.js
@@ -0,0 +1,16 @@
+import star from "../../assets/icons/Star.png";
+import { PiPerson } from "react-icons/pi";
+import home1 from "../../assets/photo/home1.png";
+import home2 from "../../assets/photo/home2.png";
+import home3 from "../../assets/photo/home3.png";
+
+const homes = [
+  {
+    img: home1,
+    name: "Sarah Nguyen",
+    person: PiPerson,
+    location: "San Francisco",
+    icon: star,
+    comment: "    ",
+  },
+];
